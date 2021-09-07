4ba0d56f686d6fbae39bf8545d7318dc3e1a79d5
Gabriel Ribeiro <ribeirogabx@gmail.com>
2021-09-07 12:02:33 -0300

    feat: ensure SignUpController returns 400 if no all required body params provided

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,9 +1,20 @@
 export class SignUpController {
+  private readonly expectedBodyParams = [
+    'name',
+    'email',
+    'password',
+    'passwordConfirmation',
+  ];
+
   public async handle(httpRequest: any): Promise<any> {
-    console.log(httpRequest);
+    const paramsProvided = Object.keys(httpRequest.body);
+    const paramsNotProvided = [...this.expectedBodyParams].filter(
+      param => !paramsProvided.includes(param),
+    );
+
     return {
       statusCode: 400,
-      body: new Error('Missing param: name'),
+      body: new Error(`Missing param: ${paramsNotProvided.join(', ')}`),
     };
   }
 }
