70459a09a8952c75e0ca891d90320633aca91901
Gabriel Ribeiro <ribeirogabx@gmail.com>
2021-10-02 16:22:49 -0300

    test: ensure BcryptAdapter return a hash on success

--- src/infra/cryptography/BcryptAdapter.spec.ts
+++ src/infra/cryptography/BcryptAdapter.spec.ts
@@ -2,6 +2,12 @@ import bcrypt from 'bcrypt';
 
 import { BcryptAdapter } from './BcryptAdapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return 'hash';
+  },
+}));
+
 function makeSut() {
   return new BcryptAdapter();
 }
@@ -15,5 +21,13 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any-value');
 
     expect(hashSpy).toHaveBeenCalledWith('any-value', expect.anything());
+  });
+
+  it('should return a hash on success', async () => {
+    const sut = makeSut();
+
+    const hash = await sut.encrypt('any-value');
+
+    expect(hash).toBe('hash');
   });
 });
