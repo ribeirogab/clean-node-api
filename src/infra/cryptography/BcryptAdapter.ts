7bd4b452d3f4f99d0a57b443879d5fbb6909bd38
Gabriel Ribeiro <ribeirogabx@gmail.com>
2021-10-02 16:11:46 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

---
+++ src/infra/cryptography/BcryptAdapter.ts
@@ -0,0 +1,12 @@
+import { hash } from 'bcrypt';
+
+import { IEncrypter } from '../../data/interfaces/IEncrypter';
+
+export class BcryptAdapter implements IEncrypter {
+  private readonly saltOrRounds = 12;
+
+  public async encrypt(value: string): Promise<string> {
+    const hashedValue = await hash(value, this.saltOrRounds);
+    return hashedValue;
+  }
+}
